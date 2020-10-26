ddf1c496b18044f5183ebcfa9dd56496e0352e3a
jmorejon <jmorejon@ingenieria.usac.edu.gt>
2020-10-25 19:56:45 -0600

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 
 const bodyParser = require('body-parser');
+const e = require('express');
 
 const PORT = process.env.PORT || 3050;
 
@@ -12,77 +13,108 @@ app.use(bodyParser.json());
 // MySql
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
-  password: 'rootpass',
-  database: 'node20_mysql'
+  user: 'usuario',
+  password: '123',
+  database: 'usuarios'
 });
 
-// Route
-app.get('/', (req, res) => {
-  res.send('Welcome to my API!');
-});
 
-// all customers
-app.get('/customers', (req, res) => {
-  const sql = 'SELECT * FROM customers';
-
-  connection.query(sql, (error, results) => {
-    if (error) throw error;
-    if (results.length > 0) {
-      res.json(results);
-    } else {
-      res.send('Not result');
-    }
-  });
-});
-
-app.get('/customers/:id', (req, res) => {
-  const { id } = req.params;
-  const sql = `SELECT * FROM customers WHERE id = ${id}`;
+//Login 
+//Datos de usuario
+app.get('/login', (req, res) => {
+  email = req.query.email;
+  password = req.query.password;
+  const sql = `SELECT * FROM usuario WHERE email = '${email}' and password='${password}'`;
   connection.query(sql, (error, result) => {
     if (error) throw error;
 
     if (result.length > 0) {
-      res.json(result);
+      result[0].administrador == 1?result[0].administrador=true:result[0].administrador=false;
+      res.json(result[0]);
     } else {
       res.send('Not result');
     }
   });
 });
 
-app.post('/add', (req, res) => {
-  const sql = 'INSERT INTO customers SET ?';
 
+//Datos de usuario
+app.get('/jugadores/:id', (req, res) => {
+  const { id } = req.params;
+  const sql = `SELECT * FROM usuario WHERE id = ${id}`;
+  connection.query(sql, (error, result) => {
+    //if (error) throw error;
+    
+    if (!error) {
+      result[0].administrador == 1?result[0].administrador=true:result[0].administrador=false;
+      res.json(result[0]);
+    } else {
+      
+      req.status(404);
+      res.statusMessage("Usuario no encontrado");
+
+    }
+  });
+});
+
+//Actualizar usuario
+app.put('/jugadores/:id', (req, res) => {
+  const { id } = req.params;
+   const{ nombres, apellidos, email,password} = req.body;
+  admin = req.body.administrador;
+   admin == "true"?admin = 1:admin=0;
+  const sql = `UPDATE usuario SET nombres = '${nombres}', apellidos='${apellidos}', email ='${email}', password ='${password}', administrador = '${admin}' WHERE id =${id}`;
+
+  connection.query(sql, error => {
+    if (error) throw error;
+    
+    req.body.id = id;
+    admin == 1 ?admin = true:admin=false;
+    req.body.administrador = admin; 
+    res.status(201);
+    res.send(req.body);
+  });
+});
+
+
+
+//Insertar usuario
+app.post('/jugadores', (req, res) => {
+  sql = 'INSERT INTO usuario SET ?';
+
+  
   const customerObj = {
-    name: req.body.name,
-    city: req.body.city
+    nombres: req.body.nombres,
+    apellidos: req.body.apellidos,
+    email: req.body.email,
+    password:req.body.password,
+    administrador: req.body.administrador
   };
+
+  
 
   connection.query(sql, customerObj, error => {
     if (error) throw error;
-    res.send('Customer created!');
   });
-});
+ 
+  sql= 'SELECT LAST_INSERT_ID() as val';
 
-app.put('/update/:id', (req, res) => {
-  const { id } = req.params;
-  const { name, city } = req.body;
-  const sql = `UPDATE customers SET name = '${name}', city='${city}' WHERE id =${id}`;
-
-  connection.query(sql, error => {
+  connection.query(sql, (error, result)=> {
     if (error) throw error;
-    res.send('Customer updated!');
+    if (result.length > 0) {
+      res.status(201);
+      
+      val = Object.values(result[0]);
+      salida = val[0];
+      res.send(salida.toString());
+    } else {
+      res.status(406);
+      res.send('Datos no válidos');
+    }
   });
-});
 
-app.delete('/delete/:id', (req, res) => {
-  const { id } = req.params;
-  const sql = `DELETE FROM customers WHERE id= ${id}`;
 
-  connection.query(sql, error => {
-    if (error) throw error;
-    res.send('Delete customer');
-  });
+
 });
 
 // Check connect
